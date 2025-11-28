9070574cd90445d9043247a387375e608df3bd49
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2025-11-29 01:58:58 +0530

    Bring over info from previous scripts

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -3,16 +3,52 @@
 set -eu
 
 ARCH=$(uname -m)
+VERSION="25.04"
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 # pacman -Syu --noconfirm PACKAGESHERE
 
+pacman -Syu --noconfirm \
+	base-devel             \
+	cmake                  \
+	curl                   \
+	git                    \
+    wget                   \
+    libcap                 \
+    libcap-ng              \
+    libpng                 \
+    poco                   \
+    cppunit                \
+    nodejs                 \
+    npm                    \
+    chromium               \
+    python-lxml            \
+    qt6-base               \
+    qt6-webengine          \
+    qt6-webchannel
+
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
 
 # If the application needs to be manually built that has to be done down here
+
+echo "Getting Collabora Office source code for $VERSION..."
+echo "---------------------------------------------------------------"
+#wget --retry-connrefused --tries=30 "$PACKAGE_BUILDER" -O ./make-aur-package.sh
+#chmod +x ./make-aur-package.sh
+#./make-aur-package.sh --chaotic-aur android_translation_layer-git
+git clone https://github.com/CollaboraOnline/online -b distro/collabora/coda-"$VERSION" CODA
+git clone https://github.com/LibreOffice/core -b distro/collabora/coda-"$VERSION" core
+cd CODA
+./autogen.sh
+./configure --enable-qtapp --with-lo-path=../core/instdir --with-lokit-path=../core/include --enable-debug CXXFLAGS="-O2 -g -fPIC"
+make -j$(nproc)
+
+echo "We should get a coda-qt file now. Let's search for it:"
+echo "---------------------------------------------------------------"
+find . -name "coda-qt"
+
+echo "$VERSION"> ~/version
--- make-appimage.sh
+++ make-appimage.sh
@@ -3,16 +3,18 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
+VERSION="$(cat ~/version)"
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON="https://raw.githubusercontent.com/CollaboraOnline/online/refs/heads/distro/collabora/coda-$VERSION/windows/coda/Assets/logo.png"
+export OUTNAME=CODA-"$VERSION"-anylinux-"$ARCH".AppImage
+#export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export DEPLOY_QT=1
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun ./CODA/qt/coda-qt
 
 # Additional changes can be done in between here
 

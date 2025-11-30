1623c944211177fbed5874775c224d6b0f96f11a
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2025-11-30 12:43:03 +0530

    include qt5 compat as well

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -25,7 +25,9 @@ pacman -Syu --noconfirm \
     python-lxml            \
     python-polib           \
     qt6-base               \
+    qt6-declarative        \
     qt6-tools              \
+    qt6-5compat            \
     qt6-webchannel         \
     qt6-webengine          \
     wget                   
@@ -38,9 +40,6 @@ get-debloated-pkgs --add-common --prefer-nano
 
 echo "Getting Collabora Office source code for $VERSION..."
 echo "---------------------------------------------------------------"
-#wget --retry-connrefused --tries=30 "$PACKAGE_BUILDER" -O ./make-aur-package.sh
-#chmod +x ./make-aur-package.sh
-#./make-aur-package.sh --chaotic-aur android_translation_layer-git
 git clone https://github.com/CollaboraOnline/online -b distro/collabora/coda-"$VERSION" CODA
 git clone https://github.com/LibreOffice/core -b distro/collabora/coda-"$VERSION" core
 cd CODA
